89a08d83611839b707a8452333aa3203b5be9813
Hardeep Kooner <hardeep.kooner@asos.com>
2017-10-16 13:01:15 +0100

    Add skeleton code for ProductDataStoreTest
    Create method to import json file.

--- ProductService.Tests/DataStore/ProductDataStoreTest.cs
+++ ProductService.Tests/DataStore/ProductDataStoreTest.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using System.Web.Http.Results;
 using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using ProductService.Controllers;
 using ProductService.DataStore;
@@ -12,18 +15,27 @@ namespace ProductService.Tests.DataStore
     [TestFixture]
     public class ProductDataStoreTest
     {
-        private Mock<IProductsDataStore> _productAdapter;
-
-        private PlpController _plpController;
-
-        private Mock<ITelemetryLogger> _telemetryLogger;
+        private ProductDataStore _productDataStore;
 
         [SetUp]
         public void SetUp()
         {
-            this._productAdapter = new Mock<IProductsDataStore>();
-            this._plpController = new PlpController(this._productAdapter.Object);
-            this._telemetryLogger = new Mock<ITelemetryLogger>();
+            this._productDataStore = new ProductDataStore();
+//            this._productAdapter = new Mock<IProductsDataStore>();
+//            this._plpController = new PlpController(this._productAdapter.Object);
+//            this._telemetryLogger = new Mock<ITelemetryLogger>();
+        }
+
+        [Test]
+        public void Return_exact_list_of_items()
+        {
+            // call data store method to return all items and store in variable 
+            var itemsFromDataStore = this._productDataStore.GetAllItemsAsync();
+
+            // import items from json file and assign to variable
+            
+
+            // assert the 2 above are the same
         }
 
 
@@ -34,6 +46,23 @@ namespace ProductService.Tests.DataStore
             int price = 2000;
             int discountPrice = 1500;
             return new PlpItem(id, productName, imageUrl, price, discountPrice);
+        }
+        private void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("../../ProductService/DataStore/TestData.json"))
+            {
+                string json = r.ReadToEnd();
+                List<PlpItem> items = JsonConvert.DeserializeObject<List<PlpItem>>(json);
+            }
+        }
+
+}
+
+    internal class ProductDataStore : IProductsDataStore
+    {
+        public Task<IEnumerable<PlpItem>> GetAllItemsAsync()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- ProductService/Models/PLPItem.cs
+++ ProductService/Models/PLPItem.cs
@@ -4,15 +4,15 @@ namespace ProductService.Models
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public string ImageUrl { get; set; }
+        public string SpashImgUrl { get; set; }
         public int Price { get; set; }
         public int? DiscountPrice { get; set; }
 
-        public PlpItem(int productId, string productName, string imageUrl, int price, int? discountPrice)
+        public PlpItem(int productId, string productName, string spashImgUrl, int price, int? discountPrice)
         {
             this.ProductId = productId;
             this.ProductName = productName;
-            this.ImageUrl = imageUrl;
+            this.SpashImgUrl = spashImgUrl;
             this.Price = price;
             this.DiscountPrice = discountPrice;
         }
