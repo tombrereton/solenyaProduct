fd741799b772e3def275325bfd0b19a843f52f7e
Tom Brereton <Tom.Brereton@asos.com>
2017-10-23 11:39:34 +0000

    Revert "Merged PR 7458: Update Tests to have hardcoded Json Test Data
    Update Tests to have hardcoded Json Test Data
    
    Tests now all pass without depending on reading in a file."

--- ProductService.Tests/DataStore/ProductDataStoreTest.cs
+++ ProductService.Tests/DataStore/ProductDataStoreTest.cs
@@ -32,7 +32,7 @@ namespace ProductService.Tests.DataStore
             var itemsFromDataStore = this._productDataStore.GetAllItemsAsync();
             
             // import items from json file and assign to variable
-            var result = GetItems();
+            var result = LoadJson();
 
             CollectionAssert.AreEqual(itemsFromDataStore.Result.ToList(), result);
         }
@@ -46,48 +46,14 @@ namespace ProductService.Tests.DataStore
             int discountPrice = 1500;
             return new PlpItem(id, productName, imageUrl, price, discountPrice);
         }
-
-        private static IEnumerable<PlpItem> GetItems()
+        private List<PlpItem> LoadJson()
         {
-            const string productString = @"[
-                            {   
-                                ""productID"": 123,
-                                ""productName"": ""Warehouse Side Split Roll Neck Jumper"",
-                                ""splashImgUrl"": ""./static/media/prod1-img1.jpg"",
-                                ""price"": 4600,
-                                ""discountPrice"": """"
-                            },
-                            { 
-                                ""productID"": 234,
-                                ""productName"": ""French Connection Checked Lined Harrington Jacket with Borg Collar"",
-                                ""splashImgUrl"": ""./static/media/prod2-img1.jpg"",
-                                ""price"": 6000,
-                                ""discountPrice"": """"
-                            },
-                            { 
-                                ""productID"": 345,
-                                ""productName"": ""Adidas Originals Trefoil Hoodie In Grey"",
-                                ""splashImgUrl"": ""./static/media/prod3-img1.jpg"",
-                                ""price"": 5000,
-                                ""discountPrice"": """"
-                            },
-                            {
-                                ""productID"": 456,
-                                ""productName"": ""ASOS Body With Plunge Neck Long Sleeve And Thong"",
-                                ""splashImgUrl"": ""./static/media/prod4-img1.jpg"",
-                                ""price"": 1050,
-                                ""discountPrice"": """"
-                            },
-                            { 
-                                ""productID"": 567,
-                                ""productName"": ""All Saints Oversized Zip Biker Jacket in Leather"",
-                                ""splashImgUrl"": ""./static/media/prod5-img1.jpg"",
-                                ""price"": 42000,
-                                ""discountPrice"": """"
-                            }
-                            ]";
-
-            return JsonConvert.DeserializeObject<List<PlpItem>>(productString);
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            using (StreamReader r = new StreamReader(currentDirectory + "/../../../ProductService/DataStore/TestData.json"))
+            {
+                string json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<PlpItem>>(json);
+            }
         }
 
 }
