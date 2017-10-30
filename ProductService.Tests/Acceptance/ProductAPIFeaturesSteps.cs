a1b3a887e05fe8693b1b0e1cff50005434f8ccda
Richard Adeoye <richard.adeoye@asos.com>
2017-10-30 12:14:03 +0000

    Create Acceptance test file for product API

---
+++ ProductService.Tests/Acceptance/ProductAPIFeaturesSteps.cs
@@ -0,0 +1,38 @@
+﻿namespace ProductService.Tests.Acceptance
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Web.Http.Results;
+
+    using NUnit.Framework;
+
+    using ProductService.Models;
+
+    using TechTalk.SpecFlow;
+
+    [Binding]
+    public class ProductAPIFeaturesSteps
+    {
+        [Given(@"I am in a browser")]
+        public void GivenIAmInABrowser()
+        {
+        }
+
+        [When(@"I enter the homepage url")]
+        public void WhenIEnterTheHomepageUrl()
+        {
+            string url = "team-solenya-dev-client.azurewebsites.net";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            ScenarioContext.Current.Add("request", request);
+        }
+
+        [Then(@"I should get an OK response")]
+        public void ThenIShouldGetAnOKResponse()
+        {
+            HttpWebRequest request = ScenarioContext.Current.Get<HttpWebRequest>("request");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Assert.IsInstanceOf<OkNegotiatedContentResult<List<PlpItem>>>(response);
+        }
+    }
+}
---
+++ ProductService.Tests/Features/ProductAPIFeatures.feature
@@ -0,0 +1,10 @@
+﻿Feature: ProductAPIFeatures
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+
+Scenario: Ability to access homepage
+	Given I am in a browser
+	When I enter the homepage url
+	Then I should get an OK response
